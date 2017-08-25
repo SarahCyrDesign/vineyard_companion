1993c6a3e86a35d6c0831f18768b7f75de0e363d
Sarah Cyr <sarahcyrdesign@gmail.com>
2017-08-25 23:28:33 +0000

    Completed methods in Wines Controller

---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,91 @@
+class WinesController < Sinatra::Base
+
+  get '/wines' do
+     if logged_in?
+       @user = current_user
+       @winess = Wine.all
+       erb :'/wines'
+     else
+       redirect to '/login'
+     end
+  end
+
+
+  get '/wines/new' do
+    if logged_in?
+      erb :'/wines/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+
+
+  post '/wines' do
+    if !session[:user_id]
+      redirect to '/'
+    elsif !params[:content].empty?
+      @user = User.find(session[:user_id])
+      @wine = Wine.create(name: params[:name], price_per_bottle: params[:price_per_bottle], type: params[:type], scent: params[:scent], taste: params[:taste], summary: params[:summary], rating: params[:rating])
+      @user.wines << @wine
+      redirect to 'wines/#{@wine.id}'
+    else
+      redirect to 'wines/new'
+    end
+  end
+
+
+  get '/wines/:id' do
+    if logged_in?
+      @wine = Wine.find(params[:id])
+      erb :'/wines/show'
+    else
+      redirect '/login'
+    end
+  end
+
+
+  get '/wines/:id/edit' do
+    @wine = Wine.find_by_id(params[:id])
+    if logged_in? && @wine.user_id == current_user.id
+      erb :'/wines/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+
+
+  patch '/wines/:id' do
+    @wine = Wine.find_by_id(params[:id])
+    if !session[:user_id]
+      redirect to '/'
+    elsif session[:user_id] != @wine.user_id
+      redirect to '/wines'
+    elsif params[:content].empty?
+      redirect to '/wines/#{@wine.id}/edit'
+    else
+      @wine.update(name: params[:name], price_per_bottle: params[:price_per_bottle], type: params[:type], scent: params[:scent], taste: params[:taste], summary: params[:summary], rating: params[:rating])
+      @wine.save
+      redirect to '/wines/#{@wine.id}'
+    end
+  end
+
+
+  delete '/wines/:id' do
+    if logged_in? && current_user
+      @wine = Wine.find_by_id(params[:id])
+      if @wine.user_id == current_user.id
+        @wine.delete
+        redirect to '/wines'
+      else
+        redirect to '/wines'
+      end
+     else
+      redirect to '/login'
+     end
+   end
+  end
+
+
+end
